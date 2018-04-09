1b6959715213a5d2debff8cbadc3edcdbafafa09
Ushakov Pavel <pushakov@promo.ru>
2018-04-09 18:29:56 +0300

    fix CI/DI config

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t mtemplate/nodejs .
 docker push mtemplate/nodejs
