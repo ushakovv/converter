4e9f6fd52389aa294debea2710023130a4ea1c8d
pushakov <smoking90@mail.ru>
2018-04-10 00:00:52 +0300

    edit CI/DI config

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t mtemplate/nodejs .
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t mtemplate/nodejs:$TAG .
 docker push mtemplate/nodejs
